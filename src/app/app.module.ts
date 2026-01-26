82ac1a2aa69f30aa581db83262a7e82580557822
Sujit Vaidya <esujit@gmail.com>
2026-01-25 20:54:07 -0800

    Add tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UserComponent,
+        TasksComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    exports: [
+        TasksComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {DUMMY_USERS} from "../dummy-users";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+  @Input() name!: string;
+
+}
